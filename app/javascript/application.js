7f17aab1d078a000d97a1666746024e8a0b53094
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-19 11:49:02 +0200

    Add the workflow

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+/** Set up mobile menu start */
+
+const menuOverlay = document.querySelector('#menu-overlay');
+const mobileMenuButton = document.querySelector('#mobileMenuButton');
+
+function mobileMenuToogle() {
+  menuOverlay.style.width = '100%';
+}
+
+function closeMobileMenu() {
+  menuOverlay.style.width = '0';
+}
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenuToogle();
+});
+
+const mobileMenuLinks = document.querySelectorAll('.menu-mobile li a');
+for (let i = 0; i < mobileMenuLinks.length; i += 1) {
+  mobileMenuLinks[i].addEventListener('click', () => {
+    closeMobileMenu();
+  });
+}
+
+/** Set up mobile menu finish */
