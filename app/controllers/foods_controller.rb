bc48bdcd432fafb04aa92e5519a4b91941b66daf
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-19 18:44:29 +0200

    Add the food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,6 @@
 class FoodsController < ApplicationController
-  # load_and_authorize_resource
-  before_action :set_food, only: %i[show destroy]
-
   def index
-    if user_signed_in?
-      @food = current_user.foods
-    else
-      redirect_to user_session_path
-    end
+    @foods = current_user.foods
   end
 
   def new
@@ -15,33 +8,59 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = current_user.foods.new(food_params)
+    food = current_user.foods.new(food_params)
 
-    respond_to do |format|
-      if @food.save
-        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if food.save
+      flash[:success] = 'New Food was successfully added.'
+      redirect_to foods_path
+    else
+      flash[:danger] = 'New Food adding Failed. Please try again.'
+    end
+  end
+
+  def edit
+    @food = current_user.foods.find(params[:id])
+  end
+
+  def update
+    @food = current_user.foods.find(params[:id])
+    if @food.update(food_params)
+      flash[:success] = 'Food was successfully updated.'
+      redirect_to foods_path
+    else
+      flash[:danger] = 'Food updating Failed. Please try again.'
     end
   end
 
   def destroy
-    @recipes = RecipeFood.where(food_id: @food.id)
-    @recipes.each(&:delete)
-    @food.delete
-    respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+    food = Food.find(params[:id])
+    if current_user == food.user
+      food.destroy
+      flash[:success] = 'Food was successfully deleted.'
+    else
+      flash[:danger] = 'You cant delete food that belongs to other users'
+    end
+    redirect_to root_path
+  end
+
+  def general
+    @foods = current_user.foods
+    current_user.recipes.map do |recipe|
+      recipe.recipe_foods.map do |recipe_food|
+        food = recipe_food.food
+        test = @foods.select { |f| f.name == food.name }[0]
+        test.quantity = test.quantity - recipe_food.quantity
+      end
+    end
+    @foods = @foods.select { |f| f.quantity.negative? }
+    @foods.each { |f| f.quantity *= -1 }
+    @total = 0
+    @foods.each do |food|
+      @total += (food.price * food.quantity)
     end
   end
 
-  private
-
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price)
+    params.require(:food).permit(:name, :quantity, :measurement_unit, :price)
   end
 end
