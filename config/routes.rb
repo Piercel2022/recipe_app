3e16df83ea9c3497d2f81f4e5567495ef1f6a2c3
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-04-20 00:35:45 +0200

    Add workflow

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  devise_for :users
-  # Defines the root path route ("/")
-  root to: 'foods#index'
-  resources :foods, only: [:index, :create, :new, :destroy]
-  resources :recipes, only: [:index, :show, :create, :new, :destroy] do
-    resources :recipe_foods, only: [:index, :edit, :update, :destroy, :create, :new]
-  end
-  resources :public_recipes, only: [:index]
-  resources :general_shopping_list, only: [:index]
-
-
-  devise_scope :user do 
-    authenticated :user do 
-      root 'users#index', as: :authenticated_root 
-    end
-    unauthenticated do 
-      root 'devise/sessions#new', as: :unauthenticated_root 
-    end
-  end
-=======
-=======
->>>>>>> 0444bd7 (Add workflow)
   devise_for :users, sign_out_via: [:get, :post]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  post 'authenticate', to: 'authentication#authenticate'
