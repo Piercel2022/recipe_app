48519a42261662e0f3c96219cf77a0984eb0e3cf
Newton <altontonnalumasa@gmail.com>
2023-04-21 02:31:08 +0300

    Resolve linter error

--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Food', type: :request do
   describe 'Get /index' do
     it 'returns http success' do
       @user = User.new(name: 'Newton')
-      @food = Food.new(name: 'beef', measurement_unit: 'kg', price: 220.0, quantity: 2, user_id: @user.id )
+      @food = Food.new(name: 'beef', measurement_unit: 'kg', price: 220.0, quantity: 2, user_id: @user.id)
       get foods_path
       expect(response).to have_http_status(302)
     end
